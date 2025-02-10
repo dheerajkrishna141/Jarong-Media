1e672f7fce3b199c9062d60c4df3e9e10d1e487c
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-02-10 14:23:05 -0500

    re-arranging files

--- src/main/java/com/jarongmedia_backend/service/EncryptionService.java
+++
@@ -1,9 +0,0 @@
-package com.jarongmedia_backend.service;
-
-public interface EncryptionService {
-	
-	public String EncryptPassword(String password);
-	
-	public boolean VerifyPassword(String password, String hash);
-
-}
